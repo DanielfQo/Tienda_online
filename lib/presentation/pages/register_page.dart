67f774c872f20f92cc63e4c7ddd3134651dfd7b9
DanielfQo <danielfrank12122004@gmail.com>
2025-07-02 14:18:46 -0500

    Cambios menores

--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../routes/app_routes.dart';
+import ''
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
