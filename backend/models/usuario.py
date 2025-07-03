c11eaa0f132e3bbcfe05467dcc62576e6c086da0
MichaelTL <mjticonala@gmail.com>
2025-07-02 20:24:33 -0500

    se realizo la conexion a la base de datos y se corrigieron errores

--- backend/models/usuario.py
+++ backend/models/usuario.py
@@ -2,10 +2,11 @@ from extensions import db
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
+    
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String, nullable=False)
-    correo = db.Column(db.String, unique=True, nullable=False)
-    contrasena = db.Column(db.String, nullable=False)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
+    contrasena_hash = db.Column(db.String(255), nullable=False)  # Aquí se guarda el hash de la contraseña
     rol = db.Column(db.Enum('cliente', 'admin', 'empleado', name='rol_enum'), nullable=False)
     verificado = db.Column(db.Boolean, default=False)
     tienda_id = db.Column(db.Integer, db.ForeignKey('tiendas.id'))
@@ -18,12 +19,12 @@ class Usuario(db.Model):
 class Cliente(db.Model):
     __tablename__ = 'clientes'
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
-    direccion_envio = db.Column(db.String)
-    telefono = db.Column(db.String)
+    direccion_envio = db.Column(db.String(255))
+    telefono = db.Column(db.String(20))
 
     usuario = db.relationship("Usuario", back_populates="cliente")
 
-class Admin(db.Base):
+class Admin(db.Model):
     __tablename__ = 'admins'
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
     nivel_acceso = db.Column(db.Integer)
@@ -33,7 +34,7 @@ class Admin(db.Base):
 class Empleado(db.Model):
     __tablename__ = 'empleados'
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
-    puesto = db.Column(db.String)
+    puesto = db.Column(db.String(30))
     fecha_contratacion = db.Column(db.DateTime)
 
     usuario = db.relationship("Usuario", back_populates="empleado")
