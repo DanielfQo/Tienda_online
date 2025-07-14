6456387fd18ea862e5e946bfc4db38e187aa1216
LeonardoGB29 <lgaonab@unsa.edu.pe>
2025-07-14 01:25:06 -0500

    dockerizando

--- backend/config.py
+++ backend/config.py
@@ -9,4 +9,4 @@ class Config:
         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") 
+    JWT_SECRET_KEY="123456"
